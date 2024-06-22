58d6a13b43d00fd6ab6ce3757eb1a630418813e1
Luke Burton <lukesburton19@gmail.com>
2024-06-21 22:17:21 -0600

    add critical error logging at all times

--- src/log.hpp
+++ src/log.hpp
@@ -1,13 +1,13 @@
 #pragma once
 #include "spdlog/spdlog.h"
 
+#define LOGCRITICAL(...) spdlog::critical(__VA_ARGS__)
+
 #ifndef NDEBUG
 	#define LOGINFO(...) spdlog::info(__VA_ARGS__)
 	#define LOGWARN(...) spdlog::warn(__VA_ARGS__)
 	#define LOGERROR(...) spdlog::error(__VA_ARGS__)
-	#define LOGCRITICAL(...) spdlog::critical(__VA_ARGS__)
 #else
 	#define LOGINFO(...)
 	#define LOGWARN(...)
-	#define LOGCRITICAL(...)
 #endif
