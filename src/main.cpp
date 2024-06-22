560219840fc3e02921d04f2dbf9d4449466c47a2
Luke Burton <lukesburton19@gmail.com>
2024-06-21 22:17:40 -0600

    add logging of game start failure

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,14 @@
 #include "game.hpp"
+#include "log.hpp"
 
 int main(int argc, char** argv)
 {
 	Game game;
-	game.start();
+	if (!game.start())
+	{
+		LOGCRITICAL("Failed to start game.");
+		exit(1);
+	}
+
     return 0;
 }
