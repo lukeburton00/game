ad39d1ddeba84b123e2d3aaf8bba13f75cba5090
Luke Burton <lukesburton19@gmail.com>
2024-07-04 15:23:07 -0600

    add vertex struct

---
+++ src/vertex.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "glm/glm.hpp"
+
+struct Vertex
+{
+    glm::vec3 position;
+    glm::vec3 color;
+    glm::vec2 texCoord;
+
+    Vertex(glm::vec3 positionParam, glm::vec3 colorParam, glm::vec2 texCoordParam)
+    {
+        position = positionParam;
+        texCoord = texCoordParam;
+        color = colorParam;
+    }
+};
