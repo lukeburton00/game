9f094b139262c57d36b452156daeec6ee244d56d
Luke Burton <lukesburton19@gmail.com>
2024-07-26 15:16:28 -0600

    update renderer with 3D test

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -1,10 +1,23 @@
 #include "renderer.hpp"
 
+#include <vector>
+
 #include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include "event.hpp"
+#include "game.hpp"
+#include "input.hpp"
+#include "keycodes.hpp"
 
 Renderer::Renderer()
 {
+    model = glm::mat4(1.f);
+    view = glm::mat4(1.f);
+    projection = glm::mat4(1.f);
 
+    fov = 10.f;
 }
 
 Renderer::~Renderer()
@@ -16,11 +29,58 @@ Renderer::~Renderer()
 
 void Renderer::init()
 {
+    width = Game::get().getWindow().getWidth();
+    height = Game::get().getWindow().getHeight();
+
+    Game::get().getEventDispatcher().subscribe<WindowResizeEvent>([&](const WindowResizeEvent& event)
+    {
+        width = event.width;
+        height = event.height;
+        projection = glm::perspective(glm::radians(fov), (float)event.width / (float)event.height, 0.1f, 100.0f);
+    });
+
 	//									position						  color						  texCoord
-	vertices.push_back(Vertex(glm::vec3(0.5f, 0.5f, 0.f),		glm::vec3(1.f, 0.f, 0.f),	glm::vec2(1.f, 1.f)));
-	vertices.push_back(Vertex(glm::vec3(0.5f, -0.5f, 0.f),		glm::vec3(0.f, 1.f, 0.f),	glm::vec2(1.f, 0.f)));
-	vertices.push_back(Vertex(glm::vec3(-0.5f, -0.5f, 0.f),		glm::vec3(0.f, 0.f, 1.f),	glm::vec2(0.f, 0.f)));
-	vertices.push_back(Vertex(glm::vec3(-0.5f, 0.5f, 0.f),		glm::vec3(1.f, 1.f, 0.f),	glm::vec2(0.f, 1.f)));
+	vertices.push_back(Vertex(glm::vec3(-0.5f, -0.5f, -0.5f),	glm::vec3(1.f, 1.f, 1.f),	glm::vec2(0.f, 0.f)));
+	vertices.push_back(Vertex(glm::vec3(0.5f, -0.5f, -0.5f),	glm::vec3(1.f, 1.f, 1.f),	glm::vec2(1.f, 0.f)));
+	vertices.push_back(Vertex(glm::vec3(0.5f, 0.5f, -0.5f),	    glm::vec3(1.f, 1.f, 1.f),	glm::vec2(1.f, 1.f)));
+	vertices.push_back(Vertex(glm::vec3(0.5f, 0.5f, -0.5f),	    glm::vec3(1.f, 1.f, 1.f),	glm::vec2(1.f, 1.f)));
+	vertices.push_back(Vertex(glm::vec3(-0.5f, 0.5f, -0.5f),	glm::vec3(1.f, 1.f, 1.f),	glm::vec2(0.f, 1.f)));
+	vertices.push_back(Vertex(glm::vec3(-0.5f, -0.5f, -0.5f),	glm::vec3(1.f, 1.f, 1.f),	glm::vec2(0.f, 0.f)));
+
+	vertices.push_back(Vertex(glm::vec3(-0.5f, -0.5f, 0.5f),	glm::vec3(1.f, 1.f, 1.f),	glm::vec2(0.f, 0.f)));
+	vertices.push_back(Vertex(glm::vec3(0.5f, -0.5f, 0.5f),		glm::vec3(1.f, 1.f, 1.f),	glm::vec2(1.f, 0.f)));
+	vertices.push_back(Vertex(glm::vec3(0.5f, 0.5f, 0.5f),		glm::vec3(1.f, 1.f, 1.f),	glm::vec2(1.f, 1.f)));
+	vertices.push_back(Vertex(glm::vec3(0.5f, 0.5f, 0.5f),		glm::vec3(1.f, 1.f, 1.f),	glm::vec2(1.f, 1.f)));
+	vertices.push_back(Vertex(glm::vec3(-0.5f, 0.5f, 0.5f),		glm::vec3(1.f, 1.f, 1.f),	glm::vec2(0.f, 1.f)));
+	vertices.push_back(Vertex(glm::vec3(-0.5f, -0.5f, 0.5f),	glm::vec3(1.f, 1.f, 1.f),	glm::vec2(0.f, 0.f)));
+
+	vertices.push_back(Vertex(glm::vec3(-0.5f, 0.5f, 0.5f),		glm::vec3(1.f, 1.f, 1.f),	glm::vec2(1.f, 0.f)));
+	vertices.push_back(Vertex(glm::vec3(-0.5f, 0.5f, -0.5f),	glm::vec3(1.f, 1.f, 1.f),	glm::vec2(1.f, 1.f)));
+	vertices.push_back(Vertex(glm::vec3(-0.5f, -0.5f, -0.5f),	glm::vec3(1.f, 1.f, 1.f),	glm::vec2(0.f, 1.f)));
+	vertices.push_back(Vertex(glm::vec3(-0.5f, -0.5f, -0.5f),	glm::vec3(1.f, 1.f, 1.f),	glm::vec2(0.f, 1.f)));
+	vertices.push_back(Vertex(glm::vec3(-0.5f, -0.5f, 0.5f),	glm::vec3(1.f, 1.f, 1.f),	glm::vec2(0.f, 0.f)));
+	vertices.push_back(Vertex(glm::vec3(-0.5f, 0.5f, 0.5f),		glm::vec3(1.f, 1.f, 1.f),	glm::vec2(1.f, 0.f)));
+
+	vertices.push_back(Vertex(glm::vec3(0.5f, 0.5f, 0.5f),		glm::vec3(1.f, 1.f, 1.f),	glm::vec2(1.f, 0.f)));
+	vertices.push_back(Vertex(glm::vec3(0.5f, 0.5f, -0.5f),		glm::vec3(1.f, 1.f, 1.f),	glm::vec2(1.f, 1.f)));
+	vertices.push_back(Vertex(glm::vec3(0.5f, -0.5f, -0.5f),	glm::vec3(1.f, 1.f, 1.f),	glm::vec2(0.f, 1.f)));
+	vertices.push_back(Vertex(glm::vec3(0.5f, -0.5f, -0.5f),	glm::vec3(1.f, 1.f, 1.f),	glm::vec2(0.f, 1.f)));
+	vertices.push_back(Vertex(glm::vec3(0.5f, -0.5f, 0.5f),		glm::vec3(1.f, 1.f, 1.f),	glm::vec2(0.f, 0.f)));
+	vertices.push_back(Vertex(glm::vec3(0.5f, 0.5f, 0.5f),		glm::vec3(1.f, 1.f, 1.f),	glm::vec2(1.f, 0.f)));
+
+	vertices.push_back(Vertex(glm::vec3(-0.5f, -0.5f, -0.5f),	glm::vec3(1.f, 1.f, 1.f),	glm::vec2(0.f, 1.f)));
+	vertices.push_back(Vertex(glm::vec3(0.5f, -0.5f, -0.5f),	glm::vec3(1.f, 1.f, 1.f),	glm::vec2(1.f, 1.f)));
+	vertices.push_back(Vertex(glm::vec3(0.5f, -0.5f, 0.5f),		glm::vec3(1.f, 1.f, 1.f),	glm::vec2(1.f, 0.f)));
+	vertices.push_back(Vertex(glm::vec3(0.5f, -0.5f, 0.5f),		glm::vec3(1.f, 1.f, 1.f),	glm::vec2(1.f, 0.f)));
+	vertices.push_back(Vertex(glm::vec3(-0.5f, -0.5f, 0.5f),	glm::vec3(1.f, 1.f, 1.f),	glm::vec2(0.f, 0.f)));
+	vertices.push_back(Vertex(glm::vec3(-0.5f, -0.5f, -0.5f),	glm::vec3(1.f, 1.f, 1.f),	glm::vec2(0.f, 1.f)));
+
+	vertices.push_back(Vertex(glm::vec3(-0.5f, 0.5f, -0.5f),	glm::vec3(1.f, 1.f, 1.f),	glm::vec2(0.f, 1.f)));
+	vertices.push_back(Vertex(glm::vec3(0.5f, 0.5f, -0.5f),		glm::vec3(1.f, 1.f, 1.f),	glm::vec2(1.f, 1.f)));
+	vertices.push_back(Vertex(glm::vec3(0.5f, 0.5f, 0.5f),		glm::vec3(1.f, 1.f, 1.f),	glm::vec2(1.f, 0.f)));
+	vertices.push_back(Vertex(glm::vec3(0.5f, 0.5f, 0.5f),		glm::vec3(1.f, 1.f, 1.f),	glm::vec2(1.f, 0.f)));
+	vertices.push_back(Vertex(glm::vec3(-0.5f, 0.5f, 0.5f),		glm::vec3(1.f, 1.f, 1.f),	glm::vec2(0.f, 0.f)));
+	vertices.push_back(Vertex(glm::vec3(-0.5f, 0.5f, -0.5f),	glm::vec3(1.f, 1.f, 1.f),	glm::vec2(0.f, 1.f)));
 
 	glGenVertexArrays(1, &VAO);
 	glBindVertexArray(VAO);
@@ -29,6 +89,7 @@ void Renderer::init()
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * vertices.size(), vertices.data(), GL_STATIC_DRAW);
 
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex),	(void*)offsetof(Vertex, position));
 	glEnableVertexAttribArray(0);
 
 	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex),	(void*)offsetof(Vertex, color));
@@ -42,6 +103,7 @@ void Renderer::init()
 		1, 2, 3    // second triangle
 	};
 
+	glEnable(GL_DEPTH_TEST);
 	glGenBuffers(1, &EBO);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
@@ -53,20 +115,65 @@ void Renderer::init()
 	shader = Shader::LoadFromFile("vertex.glsl", "fragment.glsl");
 	texture = Texture::LoadFromFile("container.jpg");
 	texture2 = Texture::LoadFromFile("awesomeface.jpg");
-}
 
-void Renderer::render()
-{
 	shader->Use();
 	shader->SetInt("outTexture", 0);
 	shader->SetInt("outTexture2", 1);
 
-	glActiveTexture(GL_TEXTURE0);
-	texture->Bind();
-	glActiveTexture(GL_TEXTURE1);
-	texture2->Bind();
+	view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+	projection = glm::perspective(glm::radians(fov), (float)width / (float)height, 0.1f, 100.0f);
+}
 
-	glBindVertexArray(VAO);
-	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-	glBindVertexArray(0);
+void Renderer::render()
+{
+    if (Input::isKeyPressed(Input::KeyCode::W))
+    {
+        fov++;
+        LOGINFO(fov);
+    }
+
+    if (Input::isKeyPressed(Input::KeyCode::S))
+    {
+        fov--;
+        LOGINFO(fov);
+    }
+
+    std::vector<glm::vec3> cubePositions = {
+        glm::vec3( 0.0f,  0.0f,  0.0f),
+        glm::vec3( 2.0f,  5.0f, 15.0f),
+        glm::vec3(-1.5f, -2.2f, -2.5f),
+        glm::vec3(-3.8f, -2.0f, 12.3f),
+        glm::vec3( 2.4f, -0.4f, -3.5f),
+        glm::vec3(-1.7f,  3.0f, -7.5f),
+        glm::vec3( 1.3f, -2.0f, 5.5f),
+        glm::vec3( 1.5f,  2.0f, -2.5f),
+        glm::vec3( 1.5f,  0.2f, 1.5f),
+        glm::vec3(-1.3f,  1.0f, -1.5f)
+    };
+
+   	glBindVertexArray(VAO);
+   	shader->Use();
+
+   	shader->SetMat4("view", view);
+
+   	projection = glm::perspective(glm::radians(fov), (float)width / (float)height, 0.1f, 100.0f);
+   	shader->SetMat4("projection", projection);
+
+   	glActiveTexture(GL_TEXTURE0);
+   	texture->Bind();
+   	glActiveTexture(GL_TEXTURE1);
+   	texture2->Bind();
+
+
+    for (int i = 0; i < 10; i++)
+    {
+        model = glm::mat4(1.f);
+        model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.f, 1.f, 0.f));
+       	model = glm::translate(model, cubePositions[i]);
+
+       	shader->SetMat4("model", model);
+       	glDrawArrays(GL_TRIANGLES, 0, 36);
+    }
+
+    glBindVertexArray(0);
 }
--- src/renderer.hpp
+++ src/renderer.hpp
@@ -19,6 +19,15 @@ private:
 	uint32_t VBO;
 	uint32_t VAO;
 	uint32_t EBO;
+	uint32_t width;
+	uint32_t height;
+
+	float fov;
+
+	glm::mat4 model;
+	glm::mat4 view;
+	glm::mat4 projection;
+
 	std::vector<Vertex> vertices;
 	std::shared_ptr<Shader> shader;
 	std::shared_ptr<Texture> texture;
