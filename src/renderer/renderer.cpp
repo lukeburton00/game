0cdc6dd042fca5970304bba71fc8a8f9e1c1cd80
Luke Burton <lukesburton19@gmail.com>
2024-09-01 23:39:45 -0600

    use static batch for static geometry

--- src/renderer/renderer.cpp
+++ src/renderer/renderer.cpp
@@ -6,7 +6,7 @@ Renderer::Renderer()
 {
 	m_viewMatrix = glm::mat4(1.f);
 	m_projectionMatrix = glm::mat4(1.f);
-
+	m_StaticBatch = std::make_shared<StaticBatch>();
 }
 
 Renderer::~Renderer()
@@ -15,16 +15,10 @@ Renderer::~Renderer()
 
 void Renderer::init(const std::shared_ptr<EventBus>& eventBus)
 {
-	m_VAO.Init();
-	m_VAO.Bind();
-	m_VBO.Init();
-	m_EBO.Init();
-	m_EBO.Bind();
-	m_VAO.Unbind();
 	m_Texture = Texture::LoadFromFile("container.jpg");
-	m_Texture->Bind();
 	m_Shader = Shader::LoadFromFile("batchVertex.glsl", "batchFragment.glsl");
-	m_Shader->Use();
+
+	m_StaticBatch->init(m_Texture, m_Shader);
 }
 
 void Renderer::beginBatch(Camera& camera)
@@ -33,36 +27,24 @@ void Renderer::beginBatch(Camera& camera)
 	m_viewMatrix = camera.GetViewMatrix();
 }
 
+void Renderer::pushStaticQuad(glm::vec2 position, glm::vec2 scale, glm::vec4 color, glm::vec2 textureCoords, glm::vec2 textureScale)
+{
+	m_StaticBatch->push(position, scale, color, textureCoords, textureScale);
+}
+
 void Renderer::pushQuad(glm::vec2 position, glm::vec2 scale, glm::vec4 color, glm::vec2 textureCoords, glm::vec2 textureScale)
 {
-	m_EBO.Push(m_VBO.numVertices + 0);
-	m_EBO.Push(m_VBO.numVertices + 1);
-	m_EBO.Push(m_VBO.numVertices + 3);
-	m_EBO.Push(m_VBO.numVertices + 1);
-	m_EBO.Push(m_VBO.numVertices + 2);
-	m_EBO.Push(m_VBO.numVertices + 3);
 
-    m_VBO.Push(Vertex(glm::vec2(position.x + scale.x, position.y),				glm::vec2(((textureCoords.x + 1) * textureScale.x) / m_Texture->width, ((textureCoords.y + 1) * textureScale.y) / m_Texture->height), color));
-    m_VBO.Push(Vertex(glm::vec2(position.x + scale.x, position.y + scale.y),	glm::vec2(((textureCoords.x + 1) * textureScale.x) / m_Texture->width, ((textureCoords.y) * textureScale.y) / m_Texture->height), color));
-    m_VBO.Push(Vertex(glm::vec2(position.x, position.y + scale.y),				glm::vec2(((textureCoords.x) * textureScale.x) / m_Texture->width, ((textureCoords.y) * textureScale.y) / m_Texture->height), color));
-    m_VBO.Push(Vertex(glm::vec2(position.x, position.y),						glm::vec2(((textureCoords.x) * textureScale.x) / m_Texture->width, ((textureCoords.y + 1) * textureScale.y) / m_Texture->height), color));
+}
+
+void Renderer::bufferStaticData()
+{
+	m_StaticBatch->bufferData();
 }
 
 void Renderer::flush()
 {
-	m_VAO.Bind();
-	m_VBO.BufferData();
-	m_EBO.BufferData();
-
-	m_Shader->Use();
-	m_Shader->SetMat4("projection", m_projectionMatrix);
-	m_Shader->SetMat4("view", m_viewMatrix);
-
-	glDrawElements(GL_TRIANGLES, m_EBO.GetIndexCount(), GL_UNSIGNED_INT, 0);
-
-	m_VAO.Unbind();
-	m_VBO.Flush();
-	m_EBO.Flush();
+	m_StaticBatch->draw(m_projectionMatrix, m_viewMatrix);
 }
 
 void Renderer::setTexture(std::shared_ptr<Texture> texture)
--- src/renderer/renderer.hpp
+++ src/renderer/renderer.hpp
@@ -9,6 +9,7 @@
 #include "gl_element_buffer.hpp"
 
 #include "glm/glm.hpp"
+#include "static_batch.hpp"
 
 class Renderer
 {
@@ -19,16 +20,16 @@ public:
 	void init(const std::shared_ptr<EventBus>& eventBus);
 
 	void beginBatch(Camera& camera);
+	void pushStaticQuad(glm::vec2 position, glm::vec2 scale, glm::vec4 color, glm::vec2 textureCoords, glm::vec2 textureScale);
 	void pushQuad(glm::vec2 position, glm::vec2 scale, glm::vec4 color, glm::vec2 texCoords, glm::vec2 textureScale);
+	void bufferStaticData();
 	void flush();
 
 private:
 	void setTexture(std::shared_ptr<Texture> texture);
 
 private:
-	GLVertexArray m_VAO;
-	GLVertexBuffer m_VBO;
-	GLElementBuffer m_EBO;
+	std::shared_ptr<StaticBatch> m_StaticBatch;
 	glm::mat4 m_viewMatrix;
 	glm::mat4 m_projectionMatrix;
 	std::shared_ptr<Shader> m_Shader;
